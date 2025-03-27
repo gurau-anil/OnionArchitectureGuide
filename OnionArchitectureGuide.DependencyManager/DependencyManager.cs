96b569b531b46e54b2ba7fdfa742a90298fa1d92
gurau-anil <gurau.anil@gmail.com>
2025-03-27 16:35:52 -0700

    Dependency manager

---
+++ OnionArchitectureGuide.DependencyManager/DependencyManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionArchitectureGuide.Application.Implementation;
+using OnionArchitectureGuide.Persistence;
+
+namespace OnionArchitectureGuide.DependencyManager
+{
+    public static class DependencyManager
+    {
+        public static void RegisterApplicationDependencies(this IServiceCollection services)
+        {
+            services.AddApplicationServices();
+        }
+
+        public static void RegisterInfrastructureDependencies(this IServiceCollection services, string connectionString)
+        {
+            services.AddInfrastructureServices(connectionString);
+        }
+    }
+}
